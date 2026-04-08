dd37a7ab9689ef46bd7b285e913c7a880151994a
mumuy <zhenghaole1990@126.com>
2026-04-08 23:56:01 +0800

    updated 将映射由Object转成Map，提高效率

--- src/module/map.js
+++ src/module/map.js
@@ -5,92 +5,101 @@ import _prefix from './data/prefix.js';
 import _branch from './data/branch.js';
 import _main from './data/main.js';
 import _multipie from './data/multiple.js';
+import { expandSelector } from './selector.js';
+import { objectToMap } from './utils.js';
 
-import {expandSelector} from './selector.js';
-
-let _map = { ..._multipie };
+let $map = objectToMap(_multipie);
 
 // 分支 - 前缀处理
 const prefixMap = {};
-for(const key in _prefix){
+for (const key in _prefix) {
     prefixMap[key] = {};
-    for(const selector in _prefix[key]){
-        expandSelector(selector).forEach(function(s){
+    for (const selector in _prefix[key]) {
+        expandSelector(selector).forEach(function (s) {
             prefixMap[key][s] = _prefix[key][selector];
         });
     }
 }
+
 // 分支 - 节点处理
 const branchMap = {};
-for(const selector in _branch){
-    expandSelector(selector).forEach(function(s){
+for (const selector in _branch) {
+    expandSelector(selector).forEach(function (s) {
         branchMap[s] = _branch[selector];
     });
 }
+
 // 分支 - 合并
-const getMap = function(prefixMap,branchMap){
-    const map = {};
-    for(const key in branchMap){
+const getMap = function (prefixMap, branchMap) {
+    const map = new Map();
+    for (const key in branchMap) {
         const tag = key.match(/\{.+?\}/)[0];
         const nameList = branchMap[key];
-        for(const k in prefixMap[tag]){
+        for (const k in prefixMap[tag]) {
             const prefixList = prefixMap[tag][k];
-            const newKey = key.replace(tag,k);
-            const isFilter = ['h,h','w,w','w,h','h,w'].some(pair=>(newKey.includes(pair)));
-            if(!isFilter){
+            const newKey = key.replace(tag, k);
+            const isFilter = ['h,h', 'w,w', 'w,h', 'h,w'].some(pair => newKey.includes(pair));
+            if (!isFilter) {
                 const newList = prefixList.flatMap((prefix) =>
                     nameList.map((name) => (name.includes('?') ? name.replace('?', prefix) : prefix + name))
                 );
-                if(!map[newKey]){
-                    map[newKey] = _map[newKey]||[];
-                }
-                map[newKey] = newList.concat(map[newKey]);
+                const existing = map.get(newKey) || $map.get(newKey) || [];
+                map.set(newKey, newList.concat(existing));
             }
         }
     }
     return map;
 };
-_map = {..._map,...getMap(prefixMap,branchMap)};
+
+const tempMap = getMap(prefixMap, branchMap);
+tempMap.forEach((value, key) => {
+    $map.set(key, value);
+});
 
 // 主要关系
-for(let key in _main){
-    _map[key] = [..._main[key], ...(_map[key] || [])];
+for (let key in _main) {
+    const existing = $map.get(key) || [];
+    $map.set(key, [..._main[key], ...existing]);
 }
 
 // 版权彩蛋
-_map['o']=['passer-by.com','\u4f5c\u8005'];
+$map.set('o', ['passer-by.com', '\u4f5c\u8005']);
 
 // 配偶关系
 const mateMap = {
-    'w':['妻','内','岳','岳家','丈人'],
-    'h':['夫','外','公','婆家','婆婆'],
+    'w': ['妻', '内', '岳', '岳家', '丈人'],
+    'h': ['夫', '外', '公', '婆家', '婆婆'],
 };
-const nameSet = new Set(Object.values(_map).flat());
-for(const key in _map){
-    if(key.match(/^[fm]/)||key.match(/^[olx][bs]$|^[olx][bs],[^mf]/)){      // 只对长辈或者兄弟辈匹配
-        for(const k in mateMap){
-            let newKey = k+','+key;
-            if(key.match(/[fm]/)){
-                let newKey_x = newKey.replace(/,[ol]([sb])(,[wh])?$/,',x$1$2').replace(/(,[sd])&[ol](,[wh])?$/,'$1$2');
-                if(newKey_x!=newKey&&_map[newKey_x]){       // 不扩大解释年龄
+
+const nameSet = new Set([...$map.values()].flat());
+
+$map.keys().forEach(function(key){
+    if (key.match(/^[fm]/) || key.match(/^[olx][bs]$|^[olx][bs],[^mf]/)) {
+        for (const k in mateMap) {
+            let newKey = k + ',' + key;
+            if (key.match(/[fm]/)) {
+                let newKey_x = newKey.replace(/,[ol]([sb])(,[wh])?$/, ',x$1$2').replace(/(,[sd])&[ol](,[wh])?$/, '$1$2');
+                if (newKey_x != newKey && $map.has(newKey_x)) {
                     continue;
                 }
             }
-            if(!_map[newKey]){
-                _map[newKey] = [];
+            if (!$map.has(newKey)) {
+                $map.set(newKey, []);
             }
             const prefixList = mateMap[k];
-            const nameList = _map[key];
-            prefixList.forEach(function(prefix){
-                nameList.forEach(function(name){
-                    const newName = prefix+name;
-                    if(!nameSet.has(newName)){              // 配偶组合的称呼不得与原有称呼冲突(如：妻舅!=妻子的舅舅;外舅公!=老公的舅公)
-                        _map[newKey].push(newName);
+            const nameList = $map.get(key);
+            const targetList = $map.get(newKey);
+            prefixList.forEach(function (prefix) {
+                nameList.forEach(function (name) {
+                    const newName = prefix + name;
+                    if (!nameSet.has(newName)) {
+                        targetList.push(newName);
+                        $map.set(newKey,targetList);
                     }
                 });
             });
         }
     }
-}
+});
 
-export default _map;
+export default $map;
--- src/module/mode.js
+++ src/module/mode.js
@@ -1,25 +1,30 @@
 /*
  * 模式数据
 */
-import _map from './map.js';
+import $map from './map.js';
+import { objectToMap, mergeMap } from './utils.js';
 
-let _mode = {};                             // 模式缓存
-let modeData = { ..._map };                 // 最终数据
+let modeHash = {};
+let $mode = new Map($map);
 
 // 设置模式数据
-export function setModeData(sign,data){
-    _mode[sign] = { ...(_mode[sign] || {}), ...data };
+export function setModeData(sign, data) {
+    const modeMap = modeHash[sign] || new Map();
+    modeHash[sign] = mergeMap(modeMap,objectToMap(data));
 };
 
 // 获取模式数据
-export function getModeData(sign){
-    modeData = { ..._map };
-    if(sign&&_mode[sign]){
-        for(const key in _mode[sign]){
-            modeData[key] = [].concat(_mode[sign][key],_map[key]||[]);
+export function getModeData(sign) {
+    $mode = new Map($map);
+    if (sign && modeHash[sign]) {
+        const mode = modeHash[sign];
+        for (const key in mode) {
+            const modeValue = mode[key];
+            const originalValue = $map.get(key) || [];
+            $mode.set(key, [...modeValue, ...originalValue]);
         }
     }
-    return modeData;
+    return $mode;
 };
 
-export {modeData};
+export { $mode };
